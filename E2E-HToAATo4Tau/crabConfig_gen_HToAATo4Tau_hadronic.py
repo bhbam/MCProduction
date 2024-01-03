7ee86e743ec7801014b08205edb3b8b34cebe39f
bhbam <bbbam@crimson.ua.edu>
2024-01-03 12:28:59 -0600

    managed deleated and modofied scripts

--- E2E-HToAATo4Tau/crabConfig_gen_HToAATo4Tau_hadronic.py
+++ E2E-HToAATo4Tau/crabConfig_gen_HToAATo4Tau_hadronic.py
@@ -1,9 +1,9 @@
 from CRABClient.UserUtilities import config
 config = config()
 # See parameter defintions here: https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile#CRAB_configuration_parameters
-Mass='3p7' # Mass of A is generally integer but put as string if need decimal.
+Mass='8' # Mass of A is generally integer but put as string if need decimal.
 # Local job directory will be created in:
-config.General.requestName = 'gen_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL'%Mass
+config.General.requestName = 'gen_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL_lessPerFile'%Mass
 config.General.workArea = 'crab_MC'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -11,16 +11,16 @@ config.General.transferLogs = True
 # CMS cfg file goes here:
 config.JobType.pluginName = 'PrivateMC'
 config.JobType.psetName = 'gen_HToAATo4Tau_M%s_RunIISummer20UL18_00066_1_cfg.py'%Mass
-config.Data.outputPrimaryDataset = 'gen_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL'%Mass
+config.Data.outputPrimaryDataset = 'gen_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL_lessPerFile'%Mass
 
 #config.JobType.maxMemoryMB = 2800
 
 # Define units per job here:
 config.JobType.allowUndistributedCMSSW = True
-config.JobType.eventsPerLumi=6000
+config.JobType.eventsPerLumi=500
 config.Data.splitting = 'EventBased'
-config.Data.unitsPerJob = 15000 # units: large number is given because HToaaTo4Tau has filters about 8 % eff
-NJOBS = 100
+config.Data.unitsPerJob = 1500 # units: large number is given because HToaaTo4Tau has filters about 8 % eff
+NJOBS = 1000
 config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
 config.Data.publication = True
 
--- MCProduction/README.md
+++
@@ -1,2 +0,0 @@
-# MCProduction
-git clone git@github.com:bhbam/MLAnalyzer.git #in src dir
