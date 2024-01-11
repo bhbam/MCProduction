a45d1b0081887941e0ca961713a7d1d705b0b631
bhbam <bbbam@crimson.ua.edu>
2024-01-11 16:47:43 -0600

    changed for unboosted signal samples

---
+++ E2E-HToAATo4Tau/Unboosted_crabConfig_gen_HToAATo4Tau_hadronic.py
@@ -0,0 +1,30 @@
+from CRABClient.UserUtilities import config
+config = config()
+# See parameter defintions here: https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile#CRAB_configuration_parameters
+Mass='10' # Mass of A is generally integer but put as string if need decimal.
+# Local job directory will be created in:
+config.General.requestName = 'Unboosted_gen_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL_lessPerFile'%Mass
+config.General.workArea = 'crab_MC'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+# CMS cfg file goes here:
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'Unboosted_gen_HToAATo4Tau_M%s_RunIISummer20UL18_00066_1_cfg.py'%Mass
+config.Data.outputPrimaryDataset = 'Unboosted_gen_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL_lessPerFile'%Mass
+
+#config.JobType.maxMemoryMB = 2800
+
+# Define units per job here:
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.eventsPerLumi=500
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 25000 # units: large number is given because HToaaTo4Tau has filters about 8 % eff
+NJOBS = 100
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+config.Data.publication = True
+
+# Output files will be stored in config.Site.storageSite at directory:
+# config.Data.outLFNDirBase = '/store/group/lpcml/bbbam/MCGeneration'
+config.Data.outLFNDirBase = '/store/user/bhbam/MCGeneration'
+config.Site.storageSite = 'T3_US_FNALLPC'
---
+++ E2E-HToAATo4Tau/Unboosted_crabConfig_sim_HToAATo4Tau_hadronic.py
@@ -0,0 +1,57 @@
+from CRABClient.UserUtilities import config#, getUsernameFromSiteDB
+config = config()
+# See parameter defintions here: https://twiki.cern.ch/twiki/bin/view/CMSPublic/CRAB3ConfigurationFile#CRAB_configuration_parameters
+
+# CFG = 'sim_HToAATo4Tau_RunIISummer20UL18_00066_1_cfg'
+
+# To submit to crab:
+# crab submit -c crabConfig_data.py
+# To check job status:
+# crab status -d <config.General.workArea>/<config.General.requestName>
+# To resubmit jobs:
+# crab resubmit -d <config.General.workArea>/<config.General.requestName>
+Mass = '8' #3p7,4,5,6,8,10,12,14
+# Local job directory will be created in:
+# <config.General.workArea>/<config.General.requestName>
+config.General.workArea        = 'crab_MC'
+config.General.requestName     = 'sim_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL'%Mass
+config.General.transferOutputs = True
+config.General.transferLogs    = True
+
+# CMS cfg file goes here:
+config.JobType.pluginName  = 'Analysis' # mass > 8 use this
+# config.JobType.pluginName  = 'PrivateMC' # mass <10 use this
+
+config.JobType.psetName    = 'sim_HToAATo4Tau_RunIISummer20UL18_00066_1_withPU_cfg.py' # cms cfg file for generating events
+config.JobType.maxMemoryMB = 5000 #5000
+# config.JobType.numCores = 4
+
+config.Data.inputDBS = 'phys03'
+config.JobType.allowUndistributedCMSSW = True
+# Define input and units per job here:
+if Mass=="8":
+    config.Data.inputDataset = '/Unboosted_gen_HToAATo4Tau_Hadronic_tauDR0p4_M8_ctau0To3_eta0To2p4_pythia8_2018UL_lessPerFile/bhbam-crab_Unboosted_gen_HToAATo4Tau_Hadronic_tauDR0p4_M8_ctau0To3_eta0To2p4_pythia8_2018UL_lessPerFile-32756e1c2f1fb6205fbab69b71f9d84d/USER'
+# config.Data.userInputFiles = open('MLAnalyzer/list_gen_sim_aToTauTau_m3p6To15.txt').readlines()
+
+# for m >8 use this to process all events.
+config.Data.splitting      = 'FileBased'
+config.Data.unitsPerJob    = 1  # units: as defined by config.Data.splitting
+config.Data.totalUnits     = -1 # -1: all inputs. total jobs submitted = totalUnits / unitsPerJob. cap of 10k jobs per submission
+
+config.Data.publication    = True
+
+
+# # for m < 10 use this but you have final number of events.
+# config.Data.splitting = 'EventAwareLumiBased'
+# config.Data.unitsPerJob = 1
+# NJOBS = 2
+# config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+# config.Data.totalUnits = -1
+# config.Data.outputPrimaryDataset = 'sim_HToAATo4Tau_Hadronic_tauDR0p4_M%s_ctau0To3_eta0To2p4_pythia8_2018UL'%Mass
+
+# Output files will be stored in config.Site.storageSite at directory:
+# <config.Data.outLFNDirBase>/<config.Data.outputPrimaryDataset>/<config.Data.outputDatasetTag>/
+# config.Data.outLFNDirBase = '/store/group/lpcml/bbbam/MCGeneration'
+config.Data.outLFNDirBase = '/store/user/bhbam/MCGeneration'
+config.Site.storageSite = 'T3_US_FNALLPC'
+config.Data.outputDatasetTag = config.General.requestName
